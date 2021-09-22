622e46cc42729b19ccf55f555ed8f62888370c73
David Lougheed <david.lougheed@gmail.com>
2021-09-22 13:22:35 -0400

    add defaults for asset dir, database set max content length

--- trail_guide_content_server/config.py
+++ trail_guide_content_server/config.py
@@ -1,8 +1,14 @@
 import os
+import pathlib
 
 __all__ = ["Config"]
 
 
+DEFAULT_ASSET_DIR = pathlib.Path(__file__).parent.parent.resolve() / "data" / "assets"
+DEFAULT_DB = pathlib.Path(__file__).parent.parent.resolve() / "data" / "db.sqlite3"
+
+
 class Config:
-    ASSET_DIR = os.environ.get("TGCS_ASSET_DIR", "")  # TODO: Defaults
-    DATABASE = os.environ.get("TGCS_DATABASE", "")  # TODO: Defaults
+    ASSET_DIR = os.environ.get("TGCS_ASSET_DIR", str(DEFAULT_ASSET_DIR))
+    DATABASE = os.environ.get("TGCS_DATABASE", str(DEFAULT_DB))
+    MAX_CONTENT_LENGTH = 2 * (1024 ** 2)  # 2 MB maximum upload size
