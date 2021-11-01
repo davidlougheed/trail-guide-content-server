40e1f5557328a94bbd40e961408bcb7515965bd5
David Lougheed <david.lougheed@gmail.com>
2021-11-01 11:30:01 -0400

    chore: sql syntax tweaks to make IDEs happier

--- trail_guide_content_server/schema.sql
+++ trail_guide_content_server/schema.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS sections (
 
 -- Pre-populate sections
 INSERT OR REPLACE INTO sections VALUES
-    ("red", "Red Trail", 0),
-    ("blue", "Blue Trail", 1),
-    ("orange", "Orange Trail", 2),
-    ("green", "Green Trail", 3),
-    ("other", "Main Facility and Off the Trails", 4);
+    ('red', 'Red Trail', 0),
+    ('blue', 'Blue Trail', 1),
+    ('orange', 'Orange Trail', 2),
+    ('green', 'Green Trail', 3),
+    ('other', 'Main Facility and Off the Trails', 4);
 
 
 CREATE TABLE IF NOT EXISTS categories (
@@ -19,27 +19,27 @@ CREATE TABLE IF NOT EXISTS categories (
 
 -- Pre-populate categories
 INSERT OR REPLACE INTO categories VALUES
-    ("culture"),
-    ("environment"),
-    ("research");
+    ('culture'),
+    ('environment'),
+    ('research');
 
 
 CREATE TABLE IF NOT EXISTS stations (
     id VARCHAR(63) PRIMARY KEY,
 
     title TEXT UNIQUE NOT NULL,
-    long_title TEXT NOT NULL DEFAULT "",
+    long_title TEXT NOT NULL DEFAULT '',
     subtitle TEXT NOT NULL,
 
-    coordinates_utm_zone TEXT NOT NULL DEFAULT "18N",
+    coordinates_utm_zone TEXT NOT NULL DEFAULT '18N',
     coordinates_utm_ew TEXT NOT NULL,
     coordinates_utm_ns TEXT NOT NULL,
 
     section TEXT NOT NULL,
-    category TEXT CHECK(category in ("culture", "environment", "research")),
+    category TEXT CHECK(category in ('culture', 'environment', 'research')),
 
     header_image TEXT,
-    contents TEXT NOT NULL DEFAULT "[]",  -- JSON list of content objects
+    contents TEXT NOT NULL DEFAULT '[]',  -- JSON list of content objects
 
     enabled INTEGER NOT NULL CHECK (enabled in (0, 1)),
     rank INTEGER NOT NULL CHECK (rank >= 0),
@@ -55,9 +55,10 @@ CREATE TABLE IF NOT EXISTS asset_types (
 
 -- Pre-populate asset types
 INSERT OR REPLACE INTO asset_types VALUES
-    ("image"),
-    ("audio"),
-    ("video");
+    ('image'),
+    ('audio'),
+    ('video'),
+    ('video_text_track');
 
 
 CREATE TABLE IF NOT EXISTS assets (
@@ -86,14 +87,14 @@ CREATE TABLE IF NOT EXISTS pages (
 
 -- Pre-populate pages with about page
 INSERT OR REPLACE INTO pages VALUES
-    ("about", "About", "Introduction to the Elbow Lake Interpretive App", "help-circle-outline", "", 0, 1, 0);
+    ('about', 'About', 'Introduction to the Elbow Lake Interpretive App', 'help-circle-outline', '', 0, 1, 0);
 
 
 CREATE TABLE IF NOT EXISTS modals (
     id VARCHAR(15) PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,  -- HTML
-    close_text TEXT NOT NULL DEFAULT "Close"
+    close_text TEXT NOT NULL DEFAULT 'Close'
 );
 
 
@@ -104,4 +105,4 @@ CREATE TABLE IF NOT EXISTS settings (
 
 -- Pre-populate settings
 INSERT OR REPLACE INTO settings VALUES
-    ("terms_modal", NULL);
+    ('terms_modal', NULL);
