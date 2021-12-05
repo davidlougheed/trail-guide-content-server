1215396e2736974c65acd36a691b6a131e5c5099
David Lougheed <david.lougheed@gmail.com>
2021-12-05 11:37:55 -0500

    chore: bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="trail_guide_content_server",
-    version="0.3.0",
+    version="0.3.1",
 
     python_requires="~=3.9",
     install_requires=[
