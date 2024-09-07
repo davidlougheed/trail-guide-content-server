3c07a2e3a7c761243cdcac8e76924f213f0e64d4
David Lougheed <david.lougheed@gmail.com>
2024-09-07 12:39:47 -0400

    chore: update copyright to 2024

--- trail_guide_content_server/object_schemas.py
+++ trail_guide_content_server/object_schemas.py
@@ -1,5 +1,5 @@
 # A server for hosting a trail guide mobile app's content and data.
-# Copyright (C) 2021-2023  David Lougheed
+# Copyright (C) 2021-2024  David Lougheed
 # See NOTICE for more information.
 
 import jsonschema
--- trail_guide_content_server/types.py
+++ trail_guide_content_server/types.py
@@ -1,3 +1,7 @@
+# A server for hosting a trail guide mobile app's content and data.
+# Copyright (C) 2021-2024  David Lougheed
+# See NOTICE for more information.
+
 from typing import Literal, TypedDict
 
 __all__ = [
