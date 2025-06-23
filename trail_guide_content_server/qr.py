c9cefc5a28b391f3489fe44e300eeaa70e61bcf6
David Lougheed <david.lougheed@gmail.com>
2025-06-23 09:43:04 -0400

    chore: update code copyright to 2025

--- trail_guide_content_server/qr.py
+++ trail_guide_content_server/qr.py
@@ -1,5 +1,5 @@
 # A server for hosting a trail guide mobile app's content and data.
-# Copyright (C) 2021-2024  David Lougheed
+# Copyright (C) 2021-2025  David Lougheed
 # See NOTICE for more information.
 
 import qrcode
